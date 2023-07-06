a8e5e31770469801115ac9d5d959ab4fff83b1a6
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-06 18:23:07 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 
 
 //Import module shoppind-cart
-import './shopping-cart.js'
 
-import {addItemToCard, totalPrice, totalQuantity} from './shopping-cart.js'
+/*
+
+import './shopping-cart.js'
+import {
+        addItemToCard,
+        totalPrice as price,
+        quantity,
+    } from './shopping-cart.js'
+ 
+*/    
+
+import * as shoppingCart from './shopping-cart.js'
+
+/*
 
 addItemToCard('Milk', 3);
-console.log(totalPrice);
-console.log(totalQuantity);
+console.log(price);
+console.log(quantity);
+*/
+
+
+shoppingCart.addItemToCard('Milk', 3);
--- shopping-cart.js
+++ shopping-cart.js
@@ -9,9 +9,9 @@ const cart = [];
 
 export const addItemToCard = function(product, quantity){
     cart.push({product, quantity});
-    console.log(`${product} added quantity ${quantity} in cart}`);
+    console.log(`${product} added quantity ${quantity} in cart`);
 }
 
 const totalPrice = 500;
 const totalQuantity = 10;
-export {totalPrice, totalQuantity};
+export {totalPrice, totalQuantity as quantity};
