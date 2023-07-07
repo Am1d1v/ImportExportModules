f9acd1c7136eb69f1227a7f2420a105882b9242c
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-07 19:49:25 +0300

    Add files via upload

--- shopping-cart.js
+++ shopping-cart.js
@@ -40,6 +40,8 @@ console.log('Some text');
 
 */
 
+/*
+
 const getLastPost = async function(){
     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
     const data = await res.json();
@@ -54,3 +56,35 @@ lastPostData.then(data => console.log(data));
 
 const lastPostData1 = await getLastPost();
 console.log(lastPostData1);
+
+*/
+
+
+
+const shoppingResult = (function(){
+    const cart = [];
+    const shoppingCost = 30;
+    const totalPrice = 500;
+    const totalQuantity = 10;
+
+    const addItemToCard = function(product, quantity){
+        cart.push({product, quantity});
+        console.log(`${product} added quantity ${quantity} in cart`);
+    };
+
+    const productOrderMessage = function(product){
+        cart.push({product, quantity});
+        console.log(`${product} has been ordered`);
+    }
+
+    return {
+        addItemToCard,
+        cart,
+        totalPrice,
+        totalQuantity
+    }
+})();
+
+shoppingResult.addItemToCard('Orange', 5);
+shoppingResult.addItemToCard('Banana', 5);
+console.log(shoppingResult);
